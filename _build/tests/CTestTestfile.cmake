311f8a25feaa23c0e2e580f7d2b5fbefe106916c
ch1la_bmstu <a2007kov@gmail.com>
2026-05-18 09:40:59 +0300

    fix tests

--- _build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ch1la/ch1la-bmstu/workspace/projects/lab05/tests
-# Build directory: /home/ch1la/ch1la-bmstu/workspace/projects/lab05/_build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check "/home/ch1la/ch1la-bmstu/workspace/projects/lab05/_build/tests/check")
-set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/ch1la/ch1la-bmstu/workspace/projects/lab05/tests/CMakeLists.txt;8;add_test;/home/ch1la/ch1la-bmstu/workspace/projects/lab05/tests/CMakeLists.txt;0;")
